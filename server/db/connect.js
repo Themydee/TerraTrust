693fbc738b42a5310c0298f48dc0656f3bc1bcd8
Themydee <nifetemiboy@gmail.com>
2025-04-01 10:36:59 +0100

    server setup

---
+++ server/db/connect.js
@@ -0,0 +1,11 @@
+import mongoose from mongoose 
+
+export const DB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB connection successful: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection unsuccessful", error.message)
+        process.exit(1); //1 is failure, 0 is success
+    }
+}
