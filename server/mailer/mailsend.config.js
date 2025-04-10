706f321b0dd6968069ded53cd5606e378cd205ee
Themydee <nifetemiboy@gmail.com>
2025-04-10 17:09:22 +0100

    mail verification

--- server/mailer/mailsend.config.js
+++ server/mailer/mailsend.config.js
@@ -6,13 +6,11 @@ dotenv.config();
 
 // Create a transporter using Gmail service
 const transporter = nodemailer.createTransport({
-  host:'smtp-relay.brevo.com',
-  port: 587,
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   }
 })
-  
-// xsmtpsib-8413839dfa404431c5c019a2b4163e216e527142ba2adefe3fb59959345654f9-9F7JGkxrORSVEzqp
 export default transporter;
